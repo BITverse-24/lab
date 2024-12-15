198949afc9cb788075375eae658b8bfc4eb67c59
SinisterDeveloper <krishaymanjunath@gmail.com>
2024-12-16 00:43:15 +0530

    Add OG Lab version for Armstrong Number

--- 1stSemester/C-Programming/armstrong.c
+++ 1stSemester/C-Programming/armstrong.c
@@ -1,3 +1,29 @@
+// Original Lab Version
+
+#include <math.h>
+#include <stdio.h>
+
+void main() {
+  int i, sum, num, count = 0;
+
+  printf("Armstrong Numbers between 1 and 1000");
+
+  for (i = 1; i <= 1000; i++) {
+    count = log10(i) + 1;
+    num = i;
+    sum = 0;
+    while (num != 0) {
+      sum += pow(num % 10, count);
+      num /= 10;
+    }
+
+    if (sum == i)
+      printf("%d\n", i);
+  }
+}
+
+
+
 /*
         Updated version of Armstrong Number program to receive user input
    regarding Upper Limit
@@ -14,7 +40,7 @@ void main() {
 
   printf("Armstrong Numbers between 1 and %d", n);
 
-  for (i = 0; i <= n; i++) {
+  for (i = 1; i <= n; i++) {
     count = log10(i) + 1;
     num = i;
     sum = 0;
